3754ced833c3784441758d9c853a06fb72febb5b
Miriana <miriana.mancini29@gmail.com>
2016-12-15 10:20:17 +0100

    remove tests

--- test/generateQuestion_test.js
+++
@@ -1,6 +0,0 @@
-questionGenerator = require('./template_test').questionGenerator
-
-questionGenerator.generateQuestion('grew up in', 'was died in', function(question){
-    console.log("Test del metodo generateQuestion")
-    console.log(question);
-});
--- test/template_test.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-var relationFacade = require('../facade/RelationFacade.js');
-var questionGenerator = require('../modules/QuestionGenerator.js');
-
-mongoose.connect('mongodb://localhost/TestLectorDB');
-
-module.exports = {relationFacade:relationFacade, questionGenerator: questionGenerator}
