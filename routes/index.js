ae3fd6aa81017ef6c4df7745f77ee68cd88cae19
Kidel <tano.design@gmail.com>
2016-12-15 20:43:10 +0100

    finished first iteration on every view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CrowdLector'});
+});
+
+router.post('/', function(req, res, next) {
+  // TODO: do stuff with email
+  res.render('questions', { 
+    title: 'CrowdLector', 
+    questions:["ciaoone", "pippo", "pluto", "qui, quo, qua"] // TODO: test stuff to avoid error on questions.jade
+  });
 });
 
 module.exports = router;
