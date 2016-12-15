7e710836509f95302a64ea50c428072fd496660c
Miriana <miriana.mancini29@gmail.com>
2016-12-15 10:13:33 +0100

    move QuestionGenerator in modules folder

--- test/template_test.js
+++ test/template_test.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var relationFacade = require('../facade/RelationFacade.js');
-var questionGenerator = require('../facade/QuestionGenerator.js');
+var questionGenerator = require('../modules/QuestionGenerator.js');
 
 mongoose.connect('mongodb://localhost/TestLectorDB');
 
