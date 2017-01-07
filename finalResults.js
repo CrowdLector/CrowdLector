252f490c9a47fad2503fd11cd7623ae1eda54a89
Matteo Filippi <Macr0s@MBP-di-Matteo.homenet.telecomitalia.it>
2017-01-07 18:50:58 +0100

    added new result data

--- finalResults.js
+++ finalResults.js
@@ -12,6 +12,7 @@ function countUnknowPhrases(file_relations, db_relations) {
     var count = {
         found: 0,
         countLength: [],
+        factCountFound: 0,
         notFound: 0
     };
 
@@ -27,7 +28,8 @@ function countUnknowPhrases(file_relations, db_relations) {
                             count: file_relation.lengthPhrases[file_phrase],
                             name: file_relation.name,
                             phrase: file_phrase
-                        })
+                        });
+                        count.factCountFound +=  file_relation.lengthPhrases[file_phrase]
                     }
                 })
             }
@@ -43,6 +45,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function() {
+    /*
     manEvaluatedParser.statManEvaluated("./input/man_evaluated.tsv", function (status, file) {
         PhraseFacade.stats(config.minNumberOfAnswers, config.minDiff, function (error, db) {
             console.log("Correct count original:", file.correct);
@@ -54,6 +57,8 @@ db.once('open', function() {
             var temp = countUnknowPhrases(file.relations, db.relations);
 
             console.log("Fatti non corrispondenti a frasi valutate da crowd", temp.notFound);
+            console.log("Frasi valutete", temp.found);
+            console.log("Fatti trovati", temp.factCountFound);
             console.log();
             console.log("Frasi valutate da crow non corrispondenti a fatti", countUnknowPhrases(db.relations, file.relations).notFound)
             console.log();
@@ -74,26 +79,63 @@ db.once('open', function() {
             });
 
             console.log("Relazioni presenti sul file e nel db", count_relation);
+        })
+    });*/
 
+    manEvaluatedParser.parseManEvaluated("./input/man_evaluated.tsv", function (status, results) {
+        PhraseFacade.listPositive(config.minNumberOfAnswers, config.minDiff, function (error, data){
+            var mapRelations = {};
             var relations = [];
 
-            db.relations.forEach(function (db_relation){
-                relations.push(db_relation.name);
-            });
+            results.forEach(function (result){
+                data.forEach(function (e){
+                    if (result.name == e.RelationName && e.Phrase == result.phrase){
+                        if (typeof mapRelations[result.name] == "undefined"){
+                            mapRelations[result.name] = {
+                                name: result.name,
+                                file_correct: 0,
+                                file_incorrect: 0,
+                                db_correct: 0,
+                                db_incorrect: 0
+                            }
 
-            console.log(relations);
+                            relations.push(mapRelations[result.name]);
+                        }
 
-            file.relations.forEach(function (file_relation){
-                if (relations.indexOf(file_relation.name) == -1){
-                    console.log(file_relation.name)
-                }
-            });
-
-            temp.countLength.forEach(function (t){
-                console.log(t.name, t.phrase, t.count);
+                        mapRelations[result.name].file_correct += result.countCorrect;
+                        mapRelations[result.name].file_incorrect += result.countIncorrect;
+                        mapRelations[result.name].db_correct += e.PositiveAnswerCount;
+                        mapRelations[result.name].db_incorrect += e.NegativeAnswerCount;
+                    }
+                })
             })
 
+            var file_correct = 0;
+            var file_incorrect = 0;
+            var db_correct = 0;
+            var db_incorrect = 0;
+
+            relations.forEach(function (relation){
+                console.log("Relation:", relation.name);
+                console.log("Correct count original:", relation.file_correct);
+                console.log("Incorrect count original: ", relation.file_incorrect);
+                console.log("Correct count after crowd:", relation.db_correct);
+                console.log("Incorrect count after crowd: ", relation.db_incorrect);
+                console.log();
+
+                file_correct += relation.file_correct;
+                file_incorrect += relation.file_incorrect;
+                db_correct += relation.db_correct;
+                db_incorrect += relation.db_incorrect;
+            })
+
+            console.log("Totali");
+            console.log("Correct count original:", file_correct);
+            console.log("Incorrect count original: ", file_incorrect);
+            console.log("Correct count after crowd:", db_correct);
+            console.log("Incorrect count after crowd: ", db_incorrect);
+            console.log();
         })
-    });
+    })
 })
 
